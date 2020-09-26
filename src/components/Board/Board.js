c3c42118cd78aab057a2dec76d8a8af0ab49545c
Jakub Krukowski <62674442+KrukowskiJ@users.noreply.github.com>
2020-09-26 14:00:16 +0200

    make Board and basic style

---
+++ src/components/Board/Board.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+import './style.css';
+
+class Board extends Component{
+    render() {
+        return(
+            <div className="board">
+                    <div>hellno</div>
+
+
+            </div>
+        )
+    }
+
+}
+
+export default Board;
